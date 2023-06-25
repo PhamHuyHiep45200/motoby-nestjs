51f7fdcbe1e8b73160a5ccf0174a270ce3707ca5
hiep pham <hiep.pham@tomosia.com>
2023-06-25 16:40:33 +0900

    add corrs

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,17 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.enableCors();
-  app.enableCors({
-    origin: [
-      'https://motobike-danang-app.vercel.app/',
-      'http://localhost:3000/',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  });
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useWebSocketAdapter(new IoAdapter(app));
   app.useStaticAssets(join(__dirname, '../uploads'));
